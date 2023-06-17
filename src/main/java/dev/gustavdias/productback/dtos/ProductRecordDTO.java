f975b3198d46dc72f6141c2fb48d585a97de8f31
Gustavo Dias <gustavoemersonfd@gmail.com>
2023-06-17 17:08:46 -0300

    Criação do ProductRecordDTO

---
+++ src/main/java/dev/gustavdias/productback/dtos/ProductRecordDTO.java
@@ -0,0 +1,10 @@
+package dev.gustavdias.productback.dtos;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ProductRecordDTO(@NotBlank String name,@NotNull BigDecimal value) {
+    
+}
